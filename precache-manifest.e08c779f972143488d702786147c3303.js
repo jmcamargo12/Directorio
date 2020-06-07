315999d4122a8fa0c51c764581c48dd9663436d8
jmcamargo12 <jmcamargo12@gmail.com>
2020-06-07 19:46:34 -0400

    Updates

---
+++ precache-manifest.e08c779f972143488d702786147c3303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a26717ac9db26bcb836",
+    "url": "/Directorio/static/css/main.bc277dd5.chunk.css"
+  },
+  {
+    "revision": "1a26717ac9db26bcb836",
+    "url": "/Directorio/static/js/main.20596647.chunk.js"
+  },
+  {
+    "revision": "dac328bb83d5f1772101",
+    "url": "/Directorio/static/js/runtime~main.d92a140d.js"
+  },
+  {
+    "revision": "ea93ffc0a6f933ff384a",
+    "url": "/Directorio/static/js/2.d8fd7b67.chunk.js"
+  },
+  {
+    "revision": "63087f0947d94aac499210d7b72cb83f",
+    "url": "/Directorio/index.html"
+  }
+];
